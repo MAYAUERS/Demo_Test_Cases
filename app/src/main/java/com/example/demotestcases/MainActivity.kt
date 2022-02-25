61a46f2410b0ae7c8d9722954d8fc83c52883077
MAYAUERS <meerapotdar999@gmail.com>
2022-02-25 07:50:46 +0530

    Add third Activity

--- app/src/main/java/com/example/demotestcases/MainActivity.kt
+++ app/src/main/java/com/example/demotestcases/MainActivity.kt
@@ -7,17 +7,21 @@ import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    lateinit var button: Button
-
+    lateinit var button1: Button
+    lateinit var button2: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button=findViewById(R.id.button_1)
-
+        button1=findViewById(R.id.button_1)
+        button2=findViewById(R.id.button2)
         Toast.makeText(this@MainActivity,"Hi Meera Welcome back to MainScreen",Toast.LENGTH_SHORT).show()
 
-        button.setOnClickListener {
+        button1.setOnClickListener {
             val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+        }
+        button2.setOnClickListener {
+            val intent = Intent(this,ThirdActivity::class.java)
             startActivity(intent)
         }
     }
