8deea4e2a19d65938d0ec0206fe66be5d3ba05f3
MAYAUERS <meerapotdar999@gmail.com>
2022-02-25 07:42:19 +0530

    Second Activity Added
    Add SecondActivity and it is calling from main

--- app/src/main/java/com/example/demotestcases/MainActivity.kt
+++ app/src/main/java/com/example/demotestcases/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.example.demotestcases
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button=findViewById(R.id.button_1)
 
         Toast.makeText(this@MainActivity,"Hi Meera Welcome back to MainScreen",Toast.LENGTH_SHORT).show()
+
+        button.setOnClickListener {
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
