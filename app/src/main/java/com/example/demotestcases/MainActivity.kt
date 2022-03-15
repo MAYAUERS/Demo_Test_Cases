87f720862c0803e84d54e6de1e549af13827703c
MAYAUERS <meerapotdar999@gmail.com>
2022-03-15 06:39:50 +0530

    Added FourthActivity

--- app/src/main/java/com/example/demotestcases/MainActivity.kt
+++ app/src/main/java/com/example/demotestcases/MainActivity.kt
@@ -9,11 +9,13 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
     lateinit var button1: Button
     lateinit var button2: Button
+    lateinit var button3: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button1=findViewById(R.id.button_1)
         button2=findViewById(R.id.button2)
+        button3=findViewById(R.id.button3)
         Toast.makeText(this@MainActivity,"Hi Meera Welcome back to MainScreen",Toast.LENGTH_SHORT).show()
 
         button1.setOnClickListener {
@@ -22,6 +24,11 @@ class MainActivity : AppCompatActivity() {
         }
         button2.setOnClickListener {
             val intent = Intent(this,ThirdActivity::class.java)
+            startActivity(intent)
+        }
+
+        button3.setOnClickListener {
+            val intent = Intent(this,FourthActivity::class.java)
             startActivity(intent)
         }
     }
